59c4070434ee0f1838d14bd1ffd3ddb3bb1464a7
ShraddhaTester <147248031+ShraddhaTester@users.noreply.github.com>
2023-10-07 17:27:06 +0530

    Update Githubtest.java

--- src/test/java/selenium/Githubtest.java
+++ src/test/java/selenium/Githubtest.java
@@ -4,7 +4,7 @@ public class Githubtest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("My name is Shraddha");
+		System.out.println("My name is Shraddha, Hi");
 
 	}
 
